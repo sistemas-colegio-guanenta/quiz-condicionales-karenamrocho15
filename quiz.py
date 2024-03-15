73fb84db776ab1ef6cf4837f4156af826cbc329e
sistemas <karenamorocho@colegioguanenta.edu.co>
2024-03-15 08:26:38 -0500

    se realizo el programa

--- quiz.py
+++ quiz.py
@@ -6,7 +6,16 @@ print("----- VERIFICAR TRIANGULO ----")
 print("------------------------------")
 
 # input
-
+a =float(input( "ingresa la longitud del lado a : "))
+b =float(input( "ingresa la longuitd del lado b : "))
+c =float(input( "ingresa la longuitud del lado c :"))
 # processing
+#verificacion si las longuitudes pueden formar un triangulo
+if a + b > c and a + c > b and b + c > a :
+    print("estas longuitudes puedem formar un traingulo.")
+else:
+    print("estas longuitudes no pueden formar un triangulo.")
+    
+
 
 # output
